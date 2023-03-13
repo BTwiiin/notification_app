55e6904d0088b6e59376676215d57c09fed30c68
BTwiiin <92520025+BTwiiin@users.noreply.github.com>
2023-03-13 17:20:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,3 @@ if __name__ == "__main__":
     email = Server()
 
     email.main_loop()
-    #email.add_notification( 'remindersfrompy@gmail.com', 'smtp-mail.outlook.com',
-                            #'Test Email', 'With Class')
-    #email.init_serv(email.get_notification())
-    #email.send('Test Email', 'With Class')
